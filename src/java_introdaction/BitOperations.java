43b088dcf5498c69d28ea9da6216b263a703b81e
Alex Karachun <alexkarachun42@gmail.com>
2022-10-30 23:19:39 +0200

    Refactored.

--- src/java_introdaction/BitOperations.java
+++ src/java_introdaction/BitOperations.java
@@ -1,28 +1,22 @@
 package java_introdaction;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 /**
- * right nBit value [0, 63] - all methods will return -1 in the case
- * of wrong nBit value
+ * valid nBit values [0, 63] - all methods will return -1 in the case of wrong
+ * nBit value
  */
 
 public class BitOperations {
 
 	/**
-	 * @param number - any number         
+	 * @param number - any number
 	 * @param nBit   - number of bit
-	 * @return value of bit with number nBit     
+	 * @return value of bit with number nBit
 	 */
 	static public int getBitValue(long number, int nBit) {
 		int res = -1;
-		if (checkNbit(nBit)) { 
+		if (checkNbit(nBit)) {
 			long mask = 1 << nBit;
-			if ((number & mask) == 0) {
-				res = 0;
-			} else {
-				res = 1;
-			}
+			res = ((number & mask) == 0) ? 0 : 1;
 		}
 		return res;
 	}
@@ -33,40 +27,30 @@ public class BitOperations {
 
 	/**
 	 * @param number - any number
-	 * @param nBit   - bit number 
+	 * @param nBit   - bit number
 	 * @param value  - true for 1 / false for 0
 	 * @return new number in which value of bit will have a given value
 	 */
-	static public long setBitValue(long number, int nBit, boolean value) {	
+	static public long setBitValue(long number, int nBit, boolean value) {
 		long res = -1;
 		if (checkNbit(nBit)) {
-			if (value == true) {
-				long mask = 1 << nBit;
-				res = number | mask;
-			} else {
-				long mask = ~(1 << nBit);
-				res = number & mask;
-			}
+			long mask = 1 << nBit;
+			res = value ? (number | mask) : (number & ~mask);
 		}
 		return res;
 	}
-	
+
 	/**
 	 * @param number - any number
-	 * @param nBit - bit number
+	 * @param nBit   - bit number
 	 * @return new number in which nBit'h will be replaced (old value 0 new value 1)
 	 */
 	static public long revertBitValue(long number, int nBit) {
 		long res = -1;
 		if (checkNbit(nBit)) {
-			int value = getBitValue(number, nBit);
-			if (value == 0) {
-				long mask = 1 << nBit;
-				res = number | mask;
-			} else {
-				long mask = ~(1 << nBit);
-				res = number & mask;
-			}
+			int bitValue = getBitValue(number, nBit);
+			long mask = 1 << nBit;
+			res = (bitValue == 0) ? (number | mask) : (number & ~mask);
 		}
 		return res;
 	}
